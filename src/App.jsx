fec90466f50a8cbb04e7486802ac50c20553c432
Andrew Miller <swishy42@gmail.com>
2023-07-26 21:47:33 +1200

    Added new icon for website. Testing BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import { BrowserRouter, Route, Link, Routes, Outlet } from 'react-router-dom'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
+const Home = () => <h2>Home Page</h2>;
+const About = () => <h2>About Page</h2>;
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+
+          <hr />
+
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+  )
+}
+
+export default App
+
+/* 
+const [count, setCount] = useState(0)
+
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -28,8 +65,5 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-    </>
-  )
-}
 
-export default App
+*/
