b8b9b1145119dd8d6511352d69e82fcbf4ddd225
Andrew Miller <swishy42@gmail.com>
2023-07-30 12:54:07 +1200

    Atttempting to commit 404.html to build viaa Vite.config.js instead of GitHub workflow.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
- 
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        error: '404.html', // Include the 404.html file as an additional entry point
+      },
+    },
+  },
+});
