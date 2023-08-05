e6516064297cc41f27ee6c6491941eb5d720f891
Andrew Miller <swishy42@gmail.com>
2023-08-05 12:34:14 +1200

    Added: More Skill Bars and updated their CSS. Added: More Technology Icons.

--- src/Components/SkillsBar.jsx
+++ src/Components/SkillsBar.jsx
@@ -2,13 +2,27 @@ import htmlIcon from '../assets/images/icons/technologies/html5.svg'
 import cssIcon from '../assets/images/icons/technologies/css.svg'
 import jsIcon from '../assets/images/icons/technologies/javascript.svg'
 import reactIcon from '../assets/images/icons/technologies/react.svg'
+import sqlIcon from '../assets/images/icons/technologies/sql.svg'
+import bootstrapIcon from '../assets/images/icons/technologies/bootstrap.svg'
+import rubyIcon from '../assets/images/icons/technologies/ruby.svg'
+import cSharpIcon from '../assets/images/icons/technologies/c-sharp.svg'
+import sqliteIcon from '../assets/images/icons/technologies/sqlite.svg'
+import apiIcon from '../assets/images/icons/technologies/api.svg'
+import jsonIcon from '../assets/images/icons/technologies/json.svg'
 
 /* Keys must match the values from the json data */
 const iconDictionary = {
-    html: htmlIcon,
-    css: cssIcon,
-    javascript: jsIcon,
-    react: reactIcon
+    HTML: htmlIcon,
+    CSS: cssIcon,
+    JavaScript: jsIcon,
+    React: reactIcon,
+    SQL: sqlIcon,
+    Bootstrap: bootstrapIcon,
+    Ruby: rubyIcon,
+    "C#": cSharpIcon,
+    SQLite: sqliteIcon,
+    API: apiIcon,
+    JSON: jsonIcon
 }
 
 const SkillsBar = ({ primarySkill, technologies }) => {
