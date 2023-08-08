66fb1d862b5c2a512c2bcc0db810cdba23bd08e2
Andrew Miller <swishy42@gmail.com>
2023-08-08 22:11:42 +1200

    More progress on Overview page logics. (json, new component, new assets)

--- src/Components/Overview.jsx
+++ src/Components/Overview.jsx
@@ -1,5 +1,5 @@
-// import testImage from '../assets/images/qualifications/linkedInLearning/reactJSEssentialTraining.jpg'
 import React, { useState, useEffect } from 'react';
+import ProjectOverview from './ProjectOverview';
 
 const getKeyFromSrc = (imageSrc) => {
     // extracts the fileName from a src by looking at the index of the last /, incrementing by 1 to move past it, then replacing the .jpg extension with nothing.
@@ -10,8 +10,10 @@ const Overview = ({ qualificationImages, projectData }) => {
     /* ReactJS and React Native are not very good at dynamically loading images, you can't just specify a src using data from a passed in array to the component,
     have to perform additional steps using states, asyncronous code and promises. */
 
-    const [images, setImages] = useState([]);
+    const [loadedScreenshots, setLoadedSccreenshots] = useState({});
+    const [loadedQualificationImages, setLoadedQualificationImages] = useState([]);
 
+    // Dynamically load the Qualification Images
     useEffect(() => {
         const loadImage = async (imageName) => {
             try {
@@ -26,23 +28,60 @@ const Overview = ({ qualificationImages, projectData }) => {
           if (qualificationImages) {
             Promise.all(qualificationImages.map(loadImage))
               .then(loadedImages => {
-                setImages(loadedImages.filter(imageSrc => imageSrc !== null));
+                setLoadedQualificationImages(loadedImages.filter(imageSrc => imageSrc !== null));
               })
               .catch(error => {
                 console.error('Error loading images:', error);
               });
           }
-          console.log("output: ", images);
+          // console.log("output: ", loadedQualificationImages);
         }, [qualificationImages]);
+
+        // Dynamically load the project screenshots
+        useEffect(() => {
+          // const loadImage = async (imageName) => {
+          //     try {
+          //       // the screenshots could be in various file formats, so will include the file extension in the json data for screenshots.
+          //       const imageModule = await import(`../assets/images/qualifications/linkedInLearning/${imageName}`);
+          //       return imageModule.default;
+          //     } catch (error) {
+          //       console.error(`Error loading image: ${imageName}`);
+          //       return null;
+          //     }
+          //   };
+  
+          //   if (projectData) {
+          //     projectData.map(data => {
+          //       if(data.screenshots.length > 0){
+          //         Promise.all(data.map(loadImage))
+          //         .then(loadedImages => {
+          //           setLoadedSccreenshots(loadedImages.filter(imageSrc => imageSrc !== null));
+          //         })
+          //         .catch(error => {
+          //           console.error('Error loading images:', error);
+          //         });
+          //       }
+          //     })
+          //   }
+          }, [loadedScreenshots]);
+
 
     return (
         <main className="overviewContainer">
             <h1>Test</h1>
-            {images &&
+            {projectData &&
+            <div className="projectOverviewWrap">
+              {projectData.map(data => (
+                /* render each of the projects here, passing in 1 block of project data */
+                <ProjectOverview />
+              ))}
+            </div>
+            }
+            {loadedQualificationImages &&
                 <div className="qualificationsContainer">
                     <h1>Qualifications</h1>
                     <p>These qualifications were gained to complement my primary qualifications and further strengthen my skills.</p>
-                    {images.map(imageSrc => (
+                    {loadedQualificationImages.map(imageSrc => (
                             <img key={getKeyFromSrc(imageSrc)} src={imageSrc} />
                     ))}
                 </div>
---
+++ src/Components/ProjectOverview.jsx
@@ -0,0 +1,11 @@
+// this component is responsible for rendering 1 object from the projectData key found within projectOverview.json
+const ProjectOverview = () => {
+
+    return (
+        <div className="projectContainer">
+            <p>Testing</p>
+        </div>
+    );
+}
+
+export default ProjectOverview;
