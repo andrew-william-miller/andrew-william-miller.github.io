478875a7db38fd53316061a966e1fca16d9ed725
Andrew Miller <swishy42@gmail.com>
2023-08-13 00:32:47 +1200

    Minor: ScrollToTop Component Comments.

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -1,3 +1,12 @@
+/* 
+    The purpose of this Component is to automatically scroll to the top when browsing to a new page within the ReactJS
+    Browser Router. From my testing, without this, when going from 1 page to another, e.g. the home to PHP overview page,
+    the vertical scroll position from home would transfer to the PHP overview page, resulting in the PHP Overview page loading with the page
+    scrolled down.
+    
+    From the testing I did, on tvs, monitors and mobile I only noticed this to be problem on mobile. Either way, it seems to be fixed with this implementation. 
+*/
+
 import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
